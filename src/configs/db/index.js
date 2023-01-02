5d9cb180cec2947954a471898239cf22a817f461
= <=>
2023-01-03 02:20:02 +0500

    delete workflow

--- src/configs/db/index.js
+++ src/configs/db/index.js
@@ -1,8 +1,8 @@
 module.exports = {
     DB:{
-        "MYSQL_HOST":"localhost",
+        "MYSQL_HOST":"3.111.75.204",
         "MYSQL_PORT":"3306",
-        "MYSQL_NAME":"mighty-flix",
+        "MYSQL_NAME":"rewamp",
         "MYSQL_USER":"root",
         "MYSQL_PASS":"test123"
     }
